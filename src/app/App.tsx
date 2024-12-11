7dd38cd36c63ee5e1d3d9bfcc0b1a8028ee5309c
RoecDesenvolvimentoWeb <roec.roecdesenvolvimentoweb@gmail.com>
2024-12-11 17:40:06 -0300

    feat: Add cart context provider

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 import Header from "./components/Organism/Header/Header";
+import { CartProvider } from "./lib/context/CartContext";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Header />
-      <AppRoutes />
-    </BrowserRouter>
+    <CartProvider>
+      <BrowserRouter>
+        <Header />
+        <AppRoutes />
+      </BrowserRouter>
+    </CartProvider>
   );
 };
 
