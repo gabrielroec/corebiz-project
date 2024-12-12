e997d411d40450b3013ab3e64b57c789659893dd
RoecDesenvolvimentoWeb <roec.roecdesenvolvimentoweb@gmail.com>
2024-12-12 11:49:20 -0300

    feat: Add onClick prop to component

--- src/app/components/Atoms/Cart/Cart.tsx
+++ src/app/components/Atoms/Cart/Cart.tsx
@@ -1,8 +1,8 @@
 import styles from "./Cart.module.scss";
 import cart from "../../../../assets/cartIcon.svg";
-const Cart = () => {
+const Cart = ({ onClick }: { onClick: () => void }) => {
   return (
-    <div className={styles.cart}>
+    <div className={styles.cart} onClick={onClick}>
       <img src={cart} alt="" />
       <span>0</span>
     </div>
